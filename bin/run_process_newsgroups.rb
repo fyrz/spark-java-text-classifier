af96153a137c04f45021d69eddb8a1d23007610f
fyrz <fyrgoss@gmail.com>
2015-03-26 21:00:25 +0100

    Further comments in newsgroup transformer script

--- bin/run_process_newsgroups.rb
+++ bin/run_process_newsgroups.rb
@@ -14,13 +14,16 @@
 # Newsgroups files must be converted to UTF-8 using `recode ISO-8859-1..UTF-8`
 # or a different mechanism to transform encodings.
 #
+# Usage:
+# $> cd <extracted newsgroup folder>
+# $> run script
+#
 #####################################################################################
 
 require 'pathname'
 require 'set'
 
-
-
+# Method to read contents of file into a string
 def readContents(filename)
   file = File.open(filename)
   contents = ""
@@ -31,19 +34,31 @@ def readContents(filename)
 end
 
 label_number = 1
+
+# Open the label index and the data file for writing
 File.open("label_index", 'a') do |label_index|
 File.open("out", 'a') do |data_file|
+
+  # Retrieve folders in newsgroup folder
   folders = Pathname.new(".").children.select { |c| c.directory? }
 
+  # Traverse through every newsgroup folder
   folders.each { |folder|
+    # Add a new entry into the label index
     label_index.puts "#{label_number},#{folder}"
 
+    # Traverse over the newsgroup posts
     files = Dir.glob("#{folder}/*").select{ |e| File.file? e }
     files.each { |f|
+
+      # read every file into a string and remove all line breaks
       contents = readContents(f).tr!("\n\r", "")
+
+      # put the data back into the data out file
       data_file.puts "#{label_number}|||#{contents}"
     }
 
+    # Increase label index counter
     label_number += 1
   }
 
