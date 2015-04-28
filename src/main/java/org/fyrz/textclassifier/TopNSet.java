b2c3e9dd034a9f543447d74cb310fa40ba4ba4f3
fyrz <fyrgoss@gmail.com>
2015-04-28 05:36:48 +0000

    Added generic parameter to TopNSet

--- src/main/java/org/fyrz/textclassifier/TopNSet.java
+++ src/main/java/org/fyrz/textclassifier/TopNSet.java
@@ -5,20 +5,19 @@ import java.util.TreeSet;
 import org.fyrz.textclassifier.beans.CategoryConfidence;
 
 
-public class TopNSet<Z> extends TreeSet<Z>
-{
+public class TopNSet<Z extends Comparable<?>> extends TreeSet<Z> {
     private final int size;
 
     public TopNSet(final int size) {
-        super();
-        this.size = size;
+      super();
+      this.size = size;
     }
 
     @Override
-    public boolean add(final Z e)
-    {   if (size() >= this.size) {
-          remove(last());
-        }
-        return super.add(e);
+    public boolean add(final Z e) {
+      if (size() >= this.size) {
+        remove(last());
+      }
+      return super.add(e);
     }
 }
